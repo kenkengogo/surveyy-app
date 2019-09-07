43a09a8d73830c1f1366b9940d968966d594351e
kenkengogo <open_sesami221@yahoo.co.jp>
2019-09-07 18:39:49 +0900

    fukidashi.update

--- app/assets/javascripts/fukidashi.js
+++ app/assets/javascripts/fukidashi.js
@@ -7,62 +7,8 @@ $(function(){
   })
   $('.data__fukidashi').each(function (i) {
     i = i+1;
-    $('#fukidashi'+i).hide()
-  })
-
-
-  if ($('.data__fukidashi').each(function (i) {
-    i = i+1;
-    $('#fukidashi'+i).is(':hidden')
-    })){
-    $('.data__vote').each(function(i){
-    $('#vote'+i).click (function() {
-   $('#fukidashi'+i).show()
-  })
+    $("#vote"+i).click (function() {
+   $("#fukidashi"+i).toggle()
 })
-}
-
-
-
-
-// else{
-//   $('.data__vote').each(function(i){
-//     $('#vote'+i).click (function() {
-//    $('#fukidashi'+i).hide()
-//   })
-// })
-// }
-
-
-
-
-
-
-
-
-
-
 })
-
-
-  // $('data-vote').each(function(i){
-  //   $('#vote'+i)
-  //   .on({'click': function(i) {
-  //   $('#fukidashi'+i).show();
-  //   }
-  // })
-  // }
-
-
-
-  // if (('#fukidashi').style.display="block"){
-  //   $('#vote')
-  //   .on({'click': function() {
-  //   $('#fukidashi').show();
-  //   }
-  // }
-    // else{
-    //   $('#vote')
-    //   .on({'click': function() {
-    //   $('#fukidashi').hide();
-    // }
+})
