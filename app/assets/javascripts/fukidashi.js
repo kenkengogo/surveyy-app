5133908487a0bb7c5cdfaca51ccf4fdca1860cab
kenkengogo <open_sesami221@yahoo.co.jp>
2019-09-18 15:25:13 +0900

    js.upgrade2

--- app/assets/javascripts/fukidashi.js
+++ app/assets/javascripts/fukidashi.js
@@ -1,14 +1,14 @@
-// $(function(){
-//   $('.data__vote').each(function(i){
-//       $(this).attr('id','vote' + (i+1));
-//   })
-//   $('.data__fukidashi').each(function(i){
-//     $(this).attr('id','fukidashi' + (i+1));
-//   })
-//   $('.data__fukidashi').each(function (i) {
-//     i = i+1;
-//     $("#vote"+i).click (function() {
-//    $("#fukidashi"+i).toggle()
-// })
-// })
-// })
+$(function(){
+  $('.data__vote').each(function(i){
+      $(this).attr('id','vote' + (i+1));
+  })
+  $('.data__fukidashi').each(function(i){
+    $(this).attr('id','fukidashi' + (i+1));
+  })
+  $('.data__fukidashi').each(function (i) {
+    i = i+1;
+    $("#vote"+i).click (function() {
+   $("#fukidashi"+i).toggle()
+})
+})
+})
--- app/assets/javascripts/header.js
+++ app/assets/javascripts/header.js
@@ -1,12 +1,12 @@
 
-// $(function(){
-//   $('#category').hide(),
-//   $('#menu').on({'mouseenter': function() {
-//     $('#category').stop().slideDown('normal');
-//     },
-//     'mouseleave' : function() {
-//       $('#category').stop().slideUp('normal');
-//     }
-//   })
-// })
+$(function(){
+  $('#category').hide(),
+  $('#menu').on({'mouseenter': function() {
+    $('#category').stop().slideDown('normal');
+    },
+    'mouseleave' : function() {
+      $('#category').stop().slideUp('normal');
+    }
+  })
+})
 
